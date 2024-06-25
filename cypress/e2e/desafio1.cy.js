66d5879993fd20437662e3d61216f43d8bc76d9c
andrea.perez <viviandrea04@gmail.com>
2024-06-25 18:42:11 -0300

    //Verificar que el producto ya no existe

--- cypress/e2e/desafio1.cy.js
+++ cypress/e2e/desafio1.cy.js
@@ -22,44 +22,46 @@ describe('Prueba de compra', () => {
         const productCard = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr0JxegsMPvc11HiyhgdFye3Od_NkhKcXDJQ&s';
         const productID = '18007';
 
+        //Dirigirse a Online Shop
         cy.get('#onlineshoplink').click();
-        cy.get(onlineShopPage.addproductButton).click();
-      
 
+        //Completar form de agregar producto y verificación de campos
+        cy.get(onlineShopPage.addproductButton).click();
         cy.get(onlineShopPage.productnameInput).type(productName);
         cy.get(onlineShopPage.productnameInput).should('have.value', productName);
-
         cy.get(onlineShopPage.productpriceInput).type(productPrice);
         cy.get(onlineShopPage.productpriceInput).should('have.value', productPrice);
-
         cy.get(onlineShopPage.productcardInput).type(productCard);
         cy.get(onlineShopPage.productcardInput).should('have.value', productCard);
-
         cy.get(onlineShopPage.productIDInput).type(productID);
         cy.get(onlineShopPage.productIDInput).should('have.value', productID);
-
         cy.get(onlineShopPage.createproductButton).click();
         cy.get(onlineShopPage.createproductButton).should('not.exist');
 
-
+        //Alerta de producto agregado y verificaciones
         cy.contains('p', `${productName} has been added`).should('be.visible');
         cy.get(onlineShopPage.closemodalButton).click();
         cy.get(onlineShopPage.closemodalButton).should('not.exist');
 
+        //Buscar el producto por ID
         cy.get('[data-cy="search-type"]').select('ID');
         cy.get('[data-cy="search-bar"]').type(`${productID}{enter}`);
         cy.get('[data-cy="search-bar"]').clear();
 
 
-
+        //Eliminar el producto y verificaciones
         onlineShopPage.deleteProduct(productID);
         cy.contains('p', `Are you sure you want to delete ${productName}?`).should('be.visible');
         cy.get(onlineShopPage.deleteproductModal).click();
         cy.contains('p', `${productName} has been deleted`).should('be.visible');
         cy.get(onlineShopPage.closemodalButton).click();
-    
+
+        //Buscar el producto eliminado
         cy.get('[data-cy="search-type"]').select('ID');
         cy.get('[data-cy="search-bar"]').type(`${productID}{enter}`);
+
+        //Verificar que el producto ya no existe
+        cy.get(`[data-cy="product-${productID}"]`).should('not.exist');
 
     })
 });
